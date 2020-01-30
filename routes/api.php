bab628974567998478b76aaa324730b873ac993a
Sakhawat Rifat <sakhawatrifat99@gmail.com>
2020-01-30 23:20:57 +0600

    Initial Commit

---
+++ routes/api.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Http\Request;
+Use App\Article;
+
+Route::get('articles', function() {
+    // If the Content-Type and Accept headers are set to 'application/json', 
+    // this will return a JSON structure. This will be cleaned up later.
+    return Article::all();
+});
+
+Route::get('articles/{id}', function($id) {
+    return Article::find($id);
+});
+
+Route::post('articles', function(Request $request) {
+     Article::create($request->all());
+     return 'Data Inserted by API';
+});
+
+Route::put('articles/{id}', function(Request $request, $id) {
+    $article = Article::findOrFail($id);
+    $article->update($request->all());
+
+    return $article;
+});
+
+Route::delete('articles/{id}', function($id) {
+    Article::find($id)->delete();
+
+    return 204;
+});
